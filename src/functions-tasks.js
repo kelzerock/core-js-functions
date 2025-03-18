bc8588c07952cb369dc253853bac756c8afb8355
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-03-18 16:08:35 +0300

    feat: memoize task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -120,8 +120,11 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const result = func();
+  return function a() {
+    return result;
+  };
 }
 
 /**
