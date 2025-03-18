cb13065748d9f1ab3282de811b96e9220410607f
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-03-18 16:14:07 +0300

    feat: getIdGeneratorFunction complete

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -207,8 +207,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return function a() {
+    counter += 1;
+    return counter - 1;
+  };
 }
 
 module.exports = {
